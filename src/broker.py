9b2b1d87dec77d12957ed8815961a61005778ef5
Abhilash Nanda <wishabhilash@gmail.com>
2018-03-27 22:52:06 +0530

    order placement in broker is done

--- src/broker.py
+++ src/broker.py
@@ -1,11 +1,84 @@
 from random import random
+from src.models import User, Order
 
 class Broker(object):
-    def buy(self):
-        return self._get_order_status()
+    def buy(self, account_id, instrument, quantity, price, trade_type, order_id=None):
+        '''
+        If order_id is None a new order is created else
+        buy_price and status of existing order is updated.
+        '''
+        
+        status = self._get_order_status()
+        if status == 'pass':
+            order = None
+            if order_id is None:
+                user = User.get(name=account_id)
+                order = Order(
+                    user = user,
+                    instrument = instrument,
+                    quantity = quantity,
+                    signal = 'sell',
+                    trade_type = trade_type,
+                    sell_price = price,
+                    status = 'open'
+                )
+            else:
+                order = order.get(_id=order_id)
+                order.buy_price = price
+                order.status='close'
+            
+            try:
+                order.save()
+            except Exception as e:
+                raise e
+            return {
+                'order_id': str(order._id),
+                'status': status
+            }
+        else:
+            return {
+                'order_id': None,
+                'status': status
+            }
 
-    def sell(self):
-        return self._get_order_status()
+    def sell(self, account_id, instrument, quantity, price, trade_type, order_id=None):
+        '''
+        If order_id is None a new order is created else
+        sell_price and status of existing order is updated.
+        '''
+        status = self._get_order_status()
+        if status == 'pass':
+            order = None
+            if order_id is None:
+                user = User.get(name=account_id)
+                order = Order(
+                    user = user,
+                    instrument = instrument,
+                    quantity = quantity,
+                    signal = 'sell',
+                    trade_type = trade_type,
+                    sell_price = price,
+                    status = 'open'
+                )
+            else:
+                order = order.get(_id=order_id)
+                order.sell_price = price
+                order.status='close'
+            
+            try:
+                order.save()
+            except Exception as e:
+                raise e
+            return {
+                'order_id': str(order._id),
+                'status': status
+            }
+        else:
+            return {
+                'order_id': None,
+                'status': status
+            }
+
 
     def _get_order_status(self):
         result = int(random() * 10) % 2
--- src/models.py
+++ src/models.py
@@ -12,7 +12,7 @@ mysql_db = MySQLDatabase(
 
 
 class User(Model):
-    name = CharField()
+    name = CharField(unique=True)
     session_key = UUIDField(default=uuid.uuid1())
     fund = IntegerField(default=0)
 
@@ -21,13 +21,15 @@ class User(Model):
 
 
 class Order(Model):
+    _id = UUIDField(default=uuid.uuid1(), primary_key=True)
     user = ForeignKeyField(User, backref='orders')
+    instrument = CharField()
     quantity = IntegerField()
     signal = CharField(choices=[(1,'buy'), (2, 'sell')])
     trade_type = CharField(choices=[(1, 'long'), (2, 'short')])
-    buy_price = FloatField()
-    sell_price = FloatField()
-    status = CharField(choices=[(1,'unsettled'), (2, 'settled')])
+    buy_price = FloatField(null=True)
+    sell_price = FloatField(null=True)
+    status = CharField(choices=[(1,'open'), (2, 'close')])
 
     class Meta:
         database = mysql_db
